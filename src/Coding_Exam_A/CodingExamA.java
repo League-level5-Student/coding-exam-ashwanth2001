2c478dc8a139983b1493253ffd03110c85dc74f6
Ashwanth <ashwanthmuruhathasan@gmail.com>
2019-04-10 18:43:15 -0700

    04/10/19
    Lvl 5 Test - 100% Coding

--- src/Coding_Exam_A/CodingExamA.java
+++ src/Coding_Exam_A/CodingExamA.java
@@ -1,6 +1,7 @@
 package Coding_Exam_A;
 
 import java.awt.Color;
+import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
@@ -21,6 +22,59 @@ public class CodingExamA {
 		 * 
 		 * See the Coding_Exam_A_Demo.jar for an example of what the finished product should look like.
 		 */
+		int width = 900;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		String c = sc.next();
+		int sides = sc.nextInt();
+		
+		Robot[] robots = new Robot[n];		
+		int w = (int)(Math.sqrt(n)+0.9999);
+		double angle = 360/sides;
+		int ind = 0;
+		
+		for(int i = 0; i<w; i++) {
+			for(int j = 0; j<w; j++) {
+				if(ind>=n) {
+					break;
+				}
+				robots[ind] = new Robot();
+				robots[ind].moveTo((int)(width/w*j), (int)(width/w*(i+.5)));
+				robots[ind].penDown();
+				robots[ind].setSpeed(10);
+				robots[ind].hide();
+				if(c.equalsIgnoreCase("blue")) {
+					robots[ind].setPenColor(Color.BLUE);
+				} else if(c.equalsIgnoreCase("red")) {
+					robots[ind].setPenColor(Color.RED);
+				} else if(c.equalsIgnoreCase("green")) {
+					robots[ind].setPenColor(Color.GREEN);
+				}
+				ind++;
+			}
+		}
+		robots[0].setWindowSize(900, 900);
+		
+		Thread[] threads= new Thread[n];
+		
+		for(int i = 0; i<n; i++) {
+			int in = i;
+			threads[i] = new Thread(() -> {
+				for(int j = 0; j<sides; j++) {
+					robots[in].setAngle((int)(angle*j));
+					robots[in].move(50);
+				}
+			});
+		}
+		
+		for(int i = 0; i<n; i++) {
+			threads[i].start();
+		}
+		
+		
+
 
 	}
 }
--- src/Coding_Exam_B/CodingExamB.java
+++ src/Coding_Exam_B/CodingExamB.java
@@ -1,6 +1,7 @@
 package Coding_Exam_B;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -8,35 +9,65 @@ import java.io.IOException;
 
 public class CodingExamB {
 	/*
-	 * This is a logging program for collecting TODO comments in a program. The program will scan
-	 * Through all the files in the Coding_Exam_B/classes package, and collect all the comments that start
-	 * with //TODO: and write them to their own file. See the TODO_Log_example.txt file for an idea of what 
-	 * the final file output will look like.
+	 * This is a logging program for collecting TODO comments in a program. The
+	 * program will scan Through all the files in the Coding_Exam_B/classes package,
+	 * and collect all the comments that start with //TODO: and write them to their
+	 * own file. See the TODO_Log_example.txt file for an idea of what the final
+	 * file output will look like.
 	 */
-	
-	
+
 	public static String getLoggingInfo(String fileName) {
 		/*
-		 * 1. Complete the getLoggingInfoMethod.
-		 *    The method takes in a String for a file and returns a String. 
-		 *    The method will open the file a read through it. It will then 
-		 *    take all the comments that begin with //TODO: and combine them 
-		 *    into one large String. The string will also state the file name and
-		 *    the line number for where each TODO was found. 
-		*/
-		
-		return "";
+		 * 1. Complete the getLoggingInfoMethod. The method takes in a String for a file
+		 * and returns a String. The method will open the file a read through it. It
+		 * will then take all the comments that begin with //TODO: and combine them into
+		 * one large String. The string will also state the file name and the line
+		 * number for where each TODO was found.
+		 */
+		String ret = "File: " + fileName + "\n";
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(fileName));
+			String st = br.readLine();
+			int line = 0;
+			while (st != null) {
+				line++;
+				if (st.indexOf("//TODO") != -1) {
+					ret += Integer.toString(line) + ": " + st.substring(st.indexOf("//")) + "\n";
+				}
+				st = br.readLine();
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		ret += "\n";
+
+		return ret;
 	}
-	
+
 	public static void main(String[] args) {
 		String finalLogString = getLoggingInfo("src/Coding_Exam_B/classes/Camera.java");
 		finalLogString += getLoggingInfo("src/Coding_Exam_B/classes/RayTracedImageViewer.java");
 		finalLogString += getLoggingInfo("src/Coding_Exam_B/classes/RayTracer.java");
 		finalLogString += getLoggingInfo("src/Coding_Exam_B/classes/Vector3.java");
-		
+
 		/*
-		 * 2. Write the finalLogString to a file called TODO_Log.txt. The file should match TODO_Log_example.txt. 
+		 * 2. Write the finalLogString to a file called TODO_Log.txt. The file should
+		 * match TODO_Log_example.txt.
 		 */
+
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter("src/Coding_Exam_B/TODO_Log.txt"));
+			writer.write(finalLogString);
+			writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 }
